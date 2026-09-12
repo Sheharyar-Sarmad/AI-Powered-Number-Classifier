4b7d125d8ce86ce142b09ab9a23be84493424147
Sheharyar-Sarmad <developersheharyar2010@gmail.com>
2026-09-12 18:46:11 +0500

    Updating run.py file for render

--- ML/run.py
+++ ML/run.py
@@ -1,5 +1,14 @@
-
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    # reload=True only in local dev
+    is_dev = os.environ.get("RENDER") is None
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=is_dev,
+    )
